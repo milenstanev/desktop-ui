883cb821dd92873e4e7b784280a3217af29ddc92
Milen Stanev <milen.stanev@gmail.com>
2024-08-24 14:20:58 +0300

    Remove unused dependency.

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import styles from './Header.module.css';
 import { useAppDispatch } from './hooks';
